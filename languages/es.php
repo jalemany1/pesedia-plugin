84fb36394c42a3088f786548dcc0c46603a41bf2
jalemany1 <jalemany1@dsic.upv.es>
2017-06-19 17:53:55 +0200

    Simplification of group creation (finished)

--- languages/es.php
+++ languages/es.php
@@ -40,6 +40,7 @@ return array(
 	'groups:policy:public' => 'Público: Todos pueden ver el grupo, los miembros y sus publicaciones.',
 	'groups:policy:private' => 'Privado: Todos pueden encontrar el grupo y ver quien esta en él. Solo los miembros pueden ver las publicaciones.',
 	'groups:policy:secret' => 'Secreto: Solo los miembros pueden encontrar el grupo y ver su contenido.',
+	'groups:types' => 'Selecciona Privacidad',
 
 	// group list
 
