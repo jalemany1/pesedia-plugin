edc8b2ecddf28dcd3cfba0f56dbfdbfac1ef942f
jalemany1 <jalemany1@dsic.upv.es>
2017-06-29 16:54:03 +0200

    Solved partially on click event to rate users.

--- views/default/js/events.js
+++ views/default/js/events.js
@@ -3,15 +3,18 @@ define(['elgg', 'jquery'], function(elgg, $) {
 	var $document = $(document);
 
 	$document.on('click', '.elgg-menu-item-add-friend > a', rate_friendship);
-	$document.on('click', '.elgg-menu-item-friend-revoke > a', rate_friendship);
-	$document.on('click', '.elgg-menu-item-friend-approve > a', rate_friendship);
-	$document.on('click', '.elgg-menu-item-friend-decline > a', rate_friendship);
-
+	// TEMPORAL SOLUTION
+	//$document.on('click', '.elgg-menu-item-friend-revoke > a', rate_friendship);
+	$('ul.elgg-list.elgg-list-entity').on('click', 'ul.elgg-menu.elgg-menu-friendship > li.elgg-menu-item-friend-revoke > a', rate_friendship);
+	//$document.on('click', '.elgg-menu-item-friend-approve > a', rate_friendship);
+	$('ul.elgg-list.elgg-list-entity').on('click', 'ul.elgg-menu.elgg-menu-friendship > li.elgg-menu-item-friend-approve > a', rate_friendship);
+	//$document.on('click', '.elgg-menu-item-friend-decline > a', rate_friendship);
+	$('ul.elgg-list.elgg-list-entity').on('click', 'ul.elgg-menu.elgg-menu-friendship > li.elgg-menu-item-friend-decline > a', rate_friendship);
+	
 
 	function rate_friendship() {
 		var lightbox = require('elgg/lightbox');
 		var spinner = require('elgg/spinner');
-
 
 		var href = $(this).attr('href');
 		var rx = '[?](guid|friend)=([^&]+)';
